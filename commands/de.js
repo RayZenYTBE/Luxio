9937e41c3db54b88786041afcb9d2e02a203cbae
AtMostFearDevs <87268716+RayZenYTBE@users.noreply.github.com>
2022-01-14 07:40:20 +0700

    Delete commands directory

--- commands/de.js
+++
@@ -1,9 +0,0 @@
-const translate = require("@iamtraction/google-translate")
-
-module.exports.run = async (client, message, args) => {
-  const query = args.join(" ");
-  if(!query) return message.reply("Please specify a text to be translated into Germanese/deutsche Sprache!");
-
-  const translated = await translate(query, { to: 'de'});
-  message.channel.send(translated.text)
-}
--- commands/es.js
+++
@@ -1,9 +0,0 @@
-const translate = require("@iamtraction/google-translate")
-
-module.exports.run = async (client, message, args) => {
-  const query = args.join(" ");
-  if(!query) return message.reply("Please specify a text to be translated into Spanish/lengua española!");
-
-  const translated = await translate(query, { to: 'es'});
-  message.channel.send(translated.text)
-}
